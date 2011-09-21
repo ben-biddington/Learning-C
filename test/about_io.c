ee8ea5e8929fb11703e10bef2908e06bb44be6bb
Ben Biddington <ben.biddington@gmail.com>
2011-09-21 22:40:11 +1200

    Describing how to monitor a file using inotify and read (no select this time).

--- test/about_io.c
+++ test/about_io.c
@@ -22,58 +22,108 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/inotify.h>
+#include <unistd.h>
 
 using std::string;
 
 class About_io : public CPPUNIT_NS::TestCase {
   CPPUNIT_TEST_SUITE(About_io);
-  CPPUNIT_TEST(you_can_monitor_a_file_for_changes_using_inotify_and_select);
+  CPPUNIT_TEST(you_can_monitor_a_file_for_changes_using_inotify);
+  CPPUNIT_TEST(you_can_add_watches);
+  CPPUNIT_TEST(you_cannot_watch_a_file_that_does_not_exist);
   CPPUNIT_TEST_SUITE_END();
 
 public:
-  void setUp(void) {}
+  void setUp(void) {
+    errno = 0;
+  }
+
   void tearDown(void) {} 
 
 protected:
-  // TODO: blocks forever seemingly every other time this is run
-  void you_can_monitor_a_file_for_changes_using_inotify_and_select() {
-    const char *the_file = "selectable_file_example";
-
-    int notifier = inotify_init();
-    int watch = inotify_add_watch(notifier, the_file, IN_MODIFY | IN_CREATE | IN_DELETE);
-    
-    fd_set file_descriptors;
-    FD_ZERO(&file_descriptors);
-    FD_SET(notifier, &file_descriptors);
-    FD_SET(watch, &file_descriptors);
+  void you_can_add_watches() {
+    const char *the_file = "example_file";
 
     touch(the_file);
 
-    int result = select(sizeof(file_descriptors)+1, &file_descriptors, NULL, NULL, NULL);
+    int notifier = inotify_init();
+    int watch = inotify_add_watch(notifier, the_file, IN_MODIFY | IN_CREATE);
 
-    char buffer[1024];
+    close(notifier);
 
-    read(notifier, &buffer, sizeof(inotify_event));
-
-    CPPUNIT_ASSERT(result);
+    CPPUNIT_ASSERT(watch > 0);
 
     unlink(the_file);
   }
 
-  void touch(const char *the_file) {
-    pid_t pid;    
+  void you_cannot_watch_a_file_that_does_not_exist() {
+    const char *any_file_that_does_not_exist = "xxx_does_not_exist_xxx";
+    
+    unlink(any_file_that_does_not_exist);
 
-    if ((pid = fork()) == 0) {
-      sleep(2);
+    int notifier = inotify_init();
+    int watch = inotify_add_watch(notifier, any_file_that_does_not_exist, IN_MODIFY | IN_CREATE);
 
-      int fd = open(the_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  
+    close(notifier);
 
-      lseek (fd, 0, SEEK_SET);
-      write (fd, "any text", 128);
+    CPPUNIT_ASSERT_EQUAL(-1, watch);
+    CPPUNIT_ASSERT_EQUAL(ENOENT, errno);
+  }
 
-      close(fd);
-      exit(0);
+  void do_not_close_watches_you_will_find_you_kill_stdout() { }
+
+  void you_can_monitor_a_file_for_changes_using_inotify() {
+    const char *the_file = "selectable_file_example";
+    
+    touch(the_file);
+    
+    pid_t pid = fork();    
+
+    bool i_am_a_child = pid == 0;
+
+    if (i_am_a_child) {
+      int notifier = inotify_init();
+      int watch = inotify_add_watch(notifier, the_file, IN_MODIFY | IN_CREATE | IN_DELETE);
+
+      if (notifier < 0) {
+	printf("\n\nError getting notifier.\n\n");
+	exit(1);
+      }
+
+      if (watch < 0) {
+	printf("\n\nFailed to get watch, ensure the file exists\n\n");
+	exit(1);
+      }
+
+      char buffer[1024];
+      int result = read(notifier, &buffer, sizeof(inotify_event));
+
+      CPPUNIT_ASSERT_MESSAGE("The result of the read was zero, i.e., nothing detected.", result);
+
+      int exit_status = result ? 0 : 1;
+      exit(exit_status);
     }
+
+    sleep(2);
+
+    touch(the_file);
+
+    int child_exit_status;
+
+    int wait_result = waitpid(pid, &child_exit_status, 0);
+
+    unlink(the_file);
+
+    CPPUNIT_ASSERT_MESSAGE("Expected the child process to've exited okay", WIFEXITED(child_exit_status));
+  }
+
+  void you_can_tell_select_has_timed_out_by_examining_the_timeval_supplied() { }
+
+  void touch(const char *the_file) {
+    int fd = open(the_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  
+    lseek (fd, 0, SEEK_SET);
+    write (fd, "xxx\n", 3);
+    close(fd);
   }
 };
 
