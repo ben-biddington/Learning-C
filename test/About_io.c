a02fcd77a66893d45009e210ad2840fdbfbd17f8
Ben Biddington <ben.biddington@gmail.com>
2011-09-21 12:05:03 +1200

    you_can_monitor_a_file_for_changes_using_inotify_and_select

--- test/About_io.c
+++ test/About_io.c
@@ -21,13 +21,13 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/inotify.h>
 
 using std::string;
 
 class About_io : public CPPUNIT_NS::TestCase {
   CPPUNIT_TEST_SUITE(About_io);
-  CPPUNIT_TEST(you_can_select_on_a_file_and_for_a_period_of_time);
-  CPPUNIT_TEST(for_example_writing_to_file_descriptor_triggers_select);
+  CPPUNIT_TEST(you_can_monitor_a_file_for_changes_using_inotify_and_select);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -35,60 +35,46 @@ public:
   void tearDown(void) {} 
 
 protected:
-  void you_can_select_on_a_file_and_for_a_period_of_time() {
-    const char *file = "selectable_file_example";
-    int fd = open (file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  
+  void you_can_monitor_a_file_for_changes_using_inotify_and_select() {
+    const char *the_file = "selectable_file_example";
+
+    int notifier = inotify_init();
 
     fd_set file_descriptors;
-
     FD_ZERO(&file_descriptors);
-    FD_SET(fd, &file_descriptors);
-
-    long timeout_seconds = 0;
-    long timeout_milliseconds = 500;
-    timeval timeout = {timeout_seconds, timeout_milliseconds};
+    FD_SET(notifier, &file_descriptors);
     
-    int result = select(1, &file_descriptors, NULL, NULL, &timeout);
+    int watch = inotify_add_watch(notifier, the_file, IN_MODIFY | IN_CREATE);
+      
+    FD_SET(watch, &file_descriptors);
 
-    close(fd);
-    unlink(file);
-  }
+    touch(the_file);
 
-  void for_example_writing_to_file_descriptor_triggers_select() {
-    const char *file = "selectable_file_example";
-    const char *expected = "Jazz and Andrew's burrito stand";
- 
-    int fd = open (file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  
-
-    fd_set file_descriptors;
-
-    FD_ZERO(&file_descriptors);
-    FD_SET(fd, &file_descriptors);
-
-    long timeout_seconds = 5;
-    long timeout_milliseconds = 500;
-    timeval timeout = {timeout_seconds, timeout_milliseconds};
-    
-    pid_t pid;
-
-    if ((pid = fork()) == 0) {
-      sleep(1);
-      lseek (fd, 0, SEEK_SET);
-      write (fd, expected, 1024);
-      exit(0);
-    }
-
-    int result = select(1, &file_descriptors, NULL, NULL, &timeout);
+    int result = select(sizeof(file_descriptors)+1, &file_descriptors, NULL, NULL, NULL);
 
     char buffer[1024];
 
-    lseek (fd, 0, SEEK_SET);
-    read(fd, &buffer, 1024);
-    
-    close(fd);
-    unlink(file);
+    read(notifier, &buffer, sizeof(inotify_event));
 
-    CPPUNIT_ASSERT_EQUAL(string(expected), string(buffer));
+    CPPUNIT_ASSERT(result);
+
+    unlink(the_file);
+  }
+
+  void touch(const char *the_file) {
+    pid_t pid;    
+
+    if ((pid = fork()) == 0) {
+      sleep(1);
+
+      int fd = open(the_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  
+
+      lseek (fd, 0, SEEK_SET);
+      write (fd, "any text", 128);
+
+      close(fd);
+      exit(0);
+    }
   }
 };
 
